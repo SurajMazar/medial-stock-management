5f4bb9c7eb065aa55d60d5b5be973a8bbfd45471
mazar suraj <suraj.mazar@gmail.com>
2021-06-29 10:13:45 +0545

    server

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,21 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="api-base-url" content="{{ route('app') }}" />
+    <meta name="api-base-url" content="{{ env("APP_URL", "") }}" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
   </head>
   <body>
     <div class="wrapper" id="root">
